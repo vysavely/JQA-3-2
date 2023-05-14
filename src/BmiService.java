6c5c5d3cedfe82ed83bc35dfc7c070f732e561e2
vysavely <s01g@ya.ru>
2023-05-14 17:24:56 +0300

    Code refactoring

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double weightKG, double growthM) {
-        double index = weightKG / (growthM*growthM);
-        return (int)index;
+    public int calculate(double weightKG, double growthM) {
+        double index = weightKG / (growthM * growthM);
+        return (int) index;
     }
 }
